8b45702dcd9586ef133a808e950d5b98d958f27f
SBSun <46879319+SBSun@users.noreply.github.com>
2023-07-16 17:58:55 +0900

    Feat : JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -35,6 +36,36 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public Member findById(String memberId) throws SQLException{
+        String sql = "SELECT * FROM member WHERE member_id = ?";
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            connection = getConnection();
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            // 무조건 한번 next 메서드를 호출해줘야 한다.
+            if(rs.next()){
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            }else{
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e){
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(connection, pstmt, rs);
+        }
+    }
+
     private void close(Connection con, Statement stmt, ResultSet rs){
 
         if(rs != null){
@@ -61,12 +92,9 @@ public class MemberRepositoryV0 {
                 log.info("error", e);
             }
         }
-
     }
 
     private Connection getConnection() {
         return DBConnectionUtil.getConnection();
     }
-
-
 }
